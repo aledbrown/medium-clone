415c28ce6865d4399ba2e6b468e4f12cae18a320
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-04-28 16:47:41 +0100

    Update registration with new fields to make it work

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,11 +2,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="flex">
+                <div class="flex flex-col sm:flex-row">
                     <div class="flex-1">
                         <h1 class="text-3xl font-bold">{{ $user->name }}</h1>
 
-                        <div class="mt-8 pr-8">
+                        <div class="mt-8 sm:pr-8">
                             @forelse($posts as $post)
                                 <x-post-item :post="$post"/>
                             @empty
@@ -20,7 +20,7 @@
 
                     </div>
 
-                    <div class="w-[320px] border-1 px-8">
+                    <div class="order-first sm:order-none mb-8 sm:mb-0 sm:w-[320px] border-1 px-8">
                         <x-user-avatar :user="$user" size="w-24 h-24" />
                         <h3 class="mt-4 text-lg font-medium text-gray-900">{{ $user->name }}</h3>
                         <p class="text-sm text-gray-500">526 followers</p>
