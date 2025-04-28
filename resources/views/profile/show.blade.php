c645442054e875cee9fd6a6b7686797850690a70
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-04-28 16:21:17 +0100

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
@@ -12,7 +6,7 @@
                     <div class="flex-1">
                         <h1 class="text-3xl font-bold">{{ $user->name }}</h1>
 
-                        <div class="mt-4">
+                        <div class="mt-8 pr-8">
                             @forelse($posts as $post)
                                 <x-post-item :post="$post"/>
                             @empty
