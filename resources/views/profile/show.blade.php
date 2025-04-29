22edf1eccc1845c0ddcaeff249b1624981f4800c
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-04-29 09:01:51 +0100

    Add followers count

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -23,7 +23,7 @@
                     <div class="order-first sm:order-none mb-8 sm:mb-0 sm:w-[320px] border-1 px-8">
                         <x-user-avatar :user="$user" size="w-24 h-24" />
                         <h3 class="mt-4 text-lg font-medium text-gray-900">{{ $user->name }}</h3>
-                        <p class="text-sm text-gray-500">526 followers</p>
+                        <p class="text-sm text-gray-500">{{ $user->followers()->count() }} {{ \Illuminate\Support\Str::plural($value = 'follower', $user->followers()->count()) }}</p>
                         <p class="mt-4 text-sm text-gray-500">{{ $user->bio }}</p>
                         <div class="mt-4">
                             <button class="block px-4 py-2 bg-emerald-800 rounded-full text-sm font-semibold text-white text-center hover:bg-black/80 transition">
